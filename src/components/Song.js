9475eff3c0d6aa406a83277d2ebe2c60e00d155c
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-06-29 14:15:18 -0700

    Added styles to song container

--- src/components/Song.js
+++ src/components/Song.js
@@ -4,8 +4,8 @@ const Song = ({currentSong}) => {
     return(
         <div className="song-container">
             <img src={currentSong.cover} alt="album cover" />
-            <h1>{currentSong.name}</h1>
-            <h1>{currentSong.artist}</h1>
+            <h2>{currentSong.name}</h2>
+            <h3>{currentSong.artist}</h3>
         </div>
     )
 }
