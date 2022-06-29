d7e43d42b04bd2985001f6d5f6563d5cedfd97ee
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-06-29 14:04:29 -0700

    created mock data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './styles/app.scss';
 import Player from './components/Player';
 import Song from './components/Song';
+import data from './util';
 
 function App() {
   return (
---
+++ src/util.js
@@ -0,0 +1,16 @@
+import { v4 as uuid } from 'uuid'
+
+function chillHop() {
+    return [
+        {
+            name: 'Full Circle',
+            cover:
+                "https://chillhop.com/wp-content/uploads/2022/05/3aaa079444cf9c1d4e0a1f53947321b29ff554c0-150x150.jpg",
+            artist: 'MOODS',
+            audio: 'https://mp3.chillhop.com/serve.php/?mp3=35507',
+            color: ['#2eb35e', '#5c7a66'],
+            id: uuid(),
+            active: true
+        }
+    ]
+}
