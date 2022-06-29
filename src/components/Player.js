03e898b2808b5df1f0d22ed410c5044ee6bc40b9
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-06-29 13:26:46 -0700

    Installed react fontawesome

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+
 
 const Player = () => {
     return(
@@ -8,7 +11,11 @@ const Player = () => {
                 <input type="range" />
                 <p>End Time</p>
             </div>
-            <div className="player-control"></div>
+            <div className="player-control">
+                <FontAwesomeIcon size='2x' className='skip-back' icon={faAngleLeft} />
+                <FontAwesomeIcon size='2x' className='play' icon={faPlay} />
+                <FontAwesomeIcon size='2x' className='skip-forward' icon={faAngleRight} />
+            </div>
         </div>
     )
 }
