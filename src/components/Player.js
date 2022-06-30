0ee6f7b99101eefa7e5908f842f6ef3462491a26
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-06-30 13:22:30 -0700

    Added time and duration to view page

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,23 +1,51 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 
-const Player = () => {
+const Player = ({currentSong, isPlaying, setIsPlaying}) => {
+    const audioRef = useRef(null);
+    const playSongHandler = () => {
+        console.log(audioRef.current.play())
+        if(isPlaying) {
+            audioRef.current.pause();
+            setIsPlaying(!isPlaying)
+        } else {
+            audioRef.current.play();
+            setIsPlaying(!isPlaying)
+        }
+    };
+
+    const [songInfo, setSongInfo] = useState({  
+        currentTime: null,
+        duration: null
+    })
+    const timeUpdateHandler = (e) => {
+        const current = e.target.currentTime;
+        const duration = e.target.duration;
+        setSongInfo({...songInfo, currentTime: current, duration})
+    }
+    const getTime = (time) => {
+        return(
+            Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+        )
+    }
+
     return(
         <div className="player">
             <div className="time-control">
-                <p>Start Time</p>
+                <p>{getTime(songInfo.currentTime)}</p>
                 <input type="range" />
-                <p>End Time</p>
+                <p>{getTime(songInfo.duration)}</p>
             </div>
             <div className="play-control">
                 <FontAwesomeIcon size='2x' className='skip-back' icon={faAngleLeft} />
-                <FontAwesomeIcon size='2x' className='play' icon={faPlay} />
+                <FontAwesomeIcon onClick={playSongHandler} size='2x' className='play' icon={faPlay} />
                 <FontAwesomeIcon size='2x' className='skip-forward' icon={faAngleRight} />
             </div>
+            <audio onTimeUpdate={timeUpdateHandler} ref={audioRef} src={currentSong.audio}></audio>
         </div>
     )
-}
+};
 
 export default Player;
